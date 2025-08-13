acdc6fc704c9ba1faa4e46ad8ad450cd8ccb080b
f00777 <felipematuranaaraya7@gmail.com>
2025-08-13 14:50:15 -0400

    13-08-2025

---
+++ app/api/upload-images/route.ts
@@ -0,0 +1,51 @@
+import { NextRequest, NextResponse } from "next/server";
+import fs from "fs";
+import path from "path";
+import sharp from "sharp";
+import db from "@/app/api/db" 
+
+export async function POST(req: NextRequest) {
+  try {
+    const formData = await req.formData();
+
+    // Obtener id y archivos
+    const id = formData.get("id") as string;
+    const files = formData.getAll("images") as File[];
+
+    if (!id || files.length === 0) {
+      return NextResponse.json({ error: "Faltan datos" }, { status: 400 });
+    }
+
+    // Crear carpeta de subida
+    const uploadDir = path.join(process.cwd(), "/public/uploads", id);
+    if (!fs.existsSync(uploadDir)) {
+      fs.mkdirSync(uploadDir, { recursive: true });
+    }
+
+    const urls: string[] = [];
+
+    // Procesar cada imagen
+    for (let i = 0; i < files.length; i++) {
+      const file = files[i];
+      const arrayBuffer = await file.arrayBuffer();
+      const buffer = Buffer.from(arrayBuffer);
+
+      const outputPath = path.join(uploadDir, `${i + 1}.webp`);
+
+      await sharp(buffer)
+        .resize(1000, 1000, { fit: "cover" })
+        .webp({ quality: 90 })
+        .toFile(outputPath);
+
+      urls.push(`/uploads/${id}/${i + 1}.webp`);
+
+      await db`UPDATE productos SET imagenes = ${urls} WHERE id = ${id}`
+
+    }
+
+    return NextResponse.json({ images: urls});
+  } catch (err) {
+    console.error(err);
+    return NextResponse.json({ error: "Error al procesar imágenes" }, { status: 500 });
+  }
+}
--- app/dashboard/productos/editar/[id]/page.tsx
+++ app/dashboard/productos/editar/[id]/page.tsx
@@ -1,10 +1,121 @@
 "use client";
 
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { useParams } from "next/navigation"
+import { useEffect, useState } from "react";
+import Spinner from "@/components/ads/Spinner";
+import { Button } from "@/components/ui/button";
 
-export default function editProduct(){
+export default function editProduct() {
+    const [inputCount, setInputCount] = useState(1);
+    const [product, setProduct] = useState<any>(null);
+    const [files, setFiles] = useState<(File | null)[]>([]);
+    const [isLoading, setIsLoading] = useState(false)
+
+    const params = useParams();
+    const id = params.id;
+
+    /* useEffect(() => {
+        async function fetchData(){
+            setIsLoading(true)
+            const response = await fetch(`/api/productos/${id}`);
+            if(!response.ok) {
+                console.error("Error fetching product data");
+                window.location.href = "/dashboard/productos";
+            }
+
+            console.log("response", response);
+
+            setProduct(await response.json());
+            setIsLoading(false)
+        }
+
+        if (!id) {
+            window.location.href = "/dashboard/productos";
+        }
+
+        fetchData();
+    }, []) */
+
+    const handleFileChange = (index: number, file: File | null) => {
+
+        const updated = [...files];
+        updated[index] = file;
+        setFiles(updated);
+
+        // Si el archivo existe y es el último input, agregamos uno nuevo
+        if (file && index === inputCount - 1) {
+            setInputCount((prev) => prev + 1);
+        }
+    }; 
+
+
+    const handleSubmit = async (e: { preventDefault: () => void; }) => {
+        e.preventDefault()
+        const formData = new FormData();
+        files.forEach((file) => {
+            if (file) formData.append("images", file);
+        });
+
+        if(!id){
+            return;
+        }
+        formData.append("id", String(id));
+        
+        const res = await fetch("/api/upload-images", {
+        method: "POST",
+        body: formData,
+        });
+
+        const data = await res.json();
+        console.log("Respuesta del servidor:", data);
+
+        window.location.href = "/dashboard/productos"
+    };
+
+    if(isLoading){
+        return (
+            <>
+                <div className="mx-auto flex items-center justify-center min-h-screen">
+                    <Spinner />
+                </div>
+            </>
+        )
+    }
+
+
     return (
-        <div>
-            Holaaa
-        </div>
+        <form className="mx-auto w-full lg:max-w-[60%] max-w-[80%] flex flex-col gap-4">
+            <Label>Nombre</Label>
+            <Input></Input>
+
+            <Label>Inventario</Label>
+            <Input></Input>
+
+            <Label>Descripción</Label>
+            <Input></Input>
+
+            {Array.from({ length: inputCount }).map((_, index) => (
+                <div key={index}>
+                <Label>
+                    Seleccione una imagen
+                </Label>
+                <Input
+                    type="file"
+                    accept="image/*"
+                    onChange={(e) =>
+                    handleFileChange(index, e.target.files?.[0] || null)
+                    }
+                />
+                </div>
+            ))}
+
+
+            <Button variant="default" onClick={handleSubmit}>Confirmar cambios</Button>
+            <Button variant="outline" onClick={(e)=>{
+                e.preventDefault()
+                window.location.href = "/dashboard/productos"}}>Cancelar</Button>
+        </form>
     )
 }
